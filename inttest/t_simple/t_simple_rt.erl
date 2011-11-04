fab4a3fde326ed12a8e0a028911fc5652a7af5d8
Tim Watson <watson.timothy@gmail.com>
2011-11-04 11:20:59 +0000

    handle transitive alt-deps

--- inttest/t_simple/t_simple_rt.erl
+++ inttest/t_simple/t_simple_rt.erl
@@ -7,24 +7,29 @@
 files() ->
     [{copy,
         "../../examples/simple", "simple"},
-     {copy, "rebar.config", "simple/rebar.config"}].
+     {copy, "rebar.config", "simple/rebar.config"},
+     {copy, "mydep.config", "mydep/rebar.config"},
+     {create, "mydep/src/mydep.app", app(mydep)}].
 
 run(Dir) ->
     retest_log:log(debug, "Running in Dir: ~s~n", [Dir]),
-    ?assertMatch({ok, _}, retest:sh("rebar install-deps -v", 
+    ?assertMatch({ok, _}, retest:sh("git init", [{dir, "mydep"}])),
+    ?assertMatch({ok, _}, retest:sh("git add rebar.config", [{dir, "mydep"}])),
+    ?assertMatch({ok, _}, retest:sh("git ci -m \"initial commit\"'", [{dir, "mydep"}])),
+    ?assertMatch({ok, _}, retest:sh("rebar install-deps -v",
                 [{dir, "simple"}])),
-    ?assertMatch(true, filelib:is_dir("simple/deps/rebar_alien_plugin")),
-    ?assertMatch(true, filelib:is_dir("simple/deps/remote_plugin_loader")),
+    ?assertMatch(true, filelib:is_dir("simple/deps/rebar_alien_plugin-0.0.2")),
+    ?assertMatch(true, filelib:is_dir("simple/deps/remote_plugin_loader-0.0.2")),
+    ?assertMatch(true, filelib:is_dir("simple/deps/mydep-1")),
     ok.
 
 %%
 %% Generate the contents of a simple .app file
 %%
-app(Name, Modules) ->
+app(Name) ->
     App = {application, Name,
            [{description, atom_to_list(Name)},
             {vsn, "1"},
-            {modules, Modules},
             {registered, []},
             {applications, [kernel, stdlib]}]},
     io_lib:format("~p.\n", [App]).
--- src/rebar_alt_deps.erl
+++ src/rebar_alt_deps.erl
@@ -22,20 +22,27 @@
 %% -----------------------------------------------------------------------------
 -module(rebar_alt_deps).
 
+-export([preprocess/2]).
 -export(['install-deps'/2]).
 -export(['post_install-deps'/2]).
 
-'install-deps'(Config, _) ->
-    case rebar_config:get_local(Config, 'alt_install_trigger', false) of
-        true ->
-            rebar_core:process_commands(['get-deps'], Config);
-        false ->
+preprocess(Config, _) ->
+    case rebar_config:get_global(alt_deps_dir, undefined) of
+        undefined ->
+            Dir = rebar_config:get_local(Config, alt_deps_dir,
+                        rebar_config:get_local(Config, deps_dir, "deps")),
+            rebar_config:set_global(alt_deps_dir, filename:absname(Dir));
+        _Dir ->
             ok
     end,
+    {ok, []}.
+
+'install-deps'(Config, _) ->
     {ok, DepsDir, Deps} = install_alt_remotes(Config),
     case file:list_dir(DepsDir) of
         {ok, Dirs} ->
-            [ process_dir(Dir, Deps, Config) || Dir <- Dirs ];
+            [ process_dir(filename:join(DepsDir, Dir),
+                            Deps, Config) || Dir <- Dirs ];
         {error, Err} ->
             rebar_log:log(warn, "Unable to process deps: ~p~n", [Err])
     end,
@@ -61,30 +68,46 @@
 %%
 
 process_dir(Dir, Deps, Config) ->
-    Origin = rebar_utils:get_cwd(),
-    %rebar_log:log(debug, "Processing ~s~n", [Dir]),
     Basename = filename:basename(Dir),
-    Command = case ([ BuildCmd || {App, _Vsn, BuildCmd} <- Deps,
-                                atom_to_list(App) == Basename andalso 
-                                is_atom(BuildCmd) ]) of
-        [] -> compile;
-        undefined -> compile;
-        [Cmd|_] -> Cmd
-    end,
-    try
-        rebar_log:log(info, "Executing ~p in ~s~n", [Command, Dir]),
-        file:set_cwd(Dir),
-        rebar_core:skip_dir(Origin),
-        rebar_core:process_commands([Command], Config),
-        erlang:erase({skip_dir, Origin})
-    catch _:Err ->
-        rebar_log:log(warn, "Unable to process directory ~s: ~p~n", [Dir, Err])
-    after
-        file:set_cwd(Origin)
+    Installed = rebar_config:get_global({?MODULE, installed}, []),
+    case lists:member(Basename, Installed) of
+        true ->
+            rebar_log:log(debug, "Skipping ~s: already processed!~n", [Basename]);
+        false ->
+            case rebar_config:get_local(Config, 'alt_install_trigger', false) of
+                true ->
+                    rebar_core:process_commands(['get-deps'], Config);
+                false ->
+                    ok
+            end,
+            Origin = rebar_utils:get_cwd(),
+            rebar_log:log(debug, "Processing ~s~n", [Dir]),
+            Command = case ([ BuildCmd || {App, _Vsn, BuildCmd} <- Deps,
+                                        atom_to_list(App) == Basename andalso
+                                        is_atom(BuildCmd) ]) of
+                [] -> compile;
+                undefined -> compile;
+                [Cmd|_] -> Cmd
+            end,
+            try
+                rebar_log:log(info, "Executing ~p in ~s~n", [Command, Dir]),
+                file:set_cwd(Dir),
+                rebar_core:skip_dir(Origin),
+                rebar_core:process_commands([Command], Config),
+                rebar_config:set_global({?MODULE, installed},
+                                        [Basename|Installed]),
+                'install-deps'(rebar_config:new(Config), undefined)
+            catch _:Err ->
+                rebar_log:log(warn, "Unable to process directory ~s: ~p~n",
+                              [Dir, Err])
+            after
+                file:set_cwd(Origin),
+                erlang:erase({skip_dir, Origin})
+            end
     end.
 
 install_alt_remotes(Config) ->
-    DepsDir = rebar_config:get_global(deps_dir, "deps"),
+    DepsDir = rebar_config:get_global(alt_deps_dir, "deps"),
     {Deps, Found} = find_deps(DepsDir, Config),
     Missing = [ Spec || {false, Spec, _} <- Found ],
     rebar_log:log(info, "Missing alt-deps: ~p~n",
@@ -105,8 +128,12 @@ find_deps(DepsDir, Config) ->
         [ load_if_possible(DepsDir, AppVsn) ||
                         AppVsn <- AltDeps ] ++ ManagedDeps}.
 
-load_if_possible(DepsDir, {App, _, _}=Spec) ->
-    BasePath = filename:join(DepsDir, atom_to_list(App)),
+load_if_possible(_, {?MODULE, _}) ->
+    code:which(?MODULE);
+load_if_possible(_, {?MODULE, _, _}) ->
+    code:which(?MODULE);
+load_if_possible(DepsDir, {App, Vsn, _}=Spec) ->
+    BasePath = filename:join(DepsDir, atom_to_list(App) ++ "-" ++ Vsn),
     rebar_log:log(debug, "Searching ~s for ~p~n", [BasePath, App]),
     case filelib:is_dir(BasePath) of
         true ->
@@ -118,11 +145,14 @@ load_if_possible(DepsDir, {App, Vsn}=Spec) ->
     BasePath = filename:join(DepsDir, atom_to_list(App)),
     rebar_log:log(debug, "Searching ~s for ~p-~s~n", [BasePath, App, Vsn]),
     case filelib:wildcard(BasePath ++ "*") of
-        [SubDir|_] ->
+        [SubDir|_]=Found ->
             case is_app_available(App, Vsn, SubDir) of
-                {true, _}=Found ->
-                    Found;
+                {true, SubDir} ->
+                    SubDir;
                 {false, Info} ->
+                    rebar_log:log(debug,
+                        "Unable to load ~p-~s. Possible matches: ~p~n",
+                        [App, Vsn, Found]),
                     {false, Spec, Info}
             end;
         _ ->
@@ -151,51 +181,69 @@ alt_load(DepsDir, {_, Vsn}=AppSpec, {Repo, Author}, Config) ->
     alt_load(DepsDir, AppSpec, {Repo, Author, Vsn}, Config);
 alt_load(DepsDir, {App, Vsn}, {Repo, Author, Tag}, Config) ->
     alt_load(DepsDir, {App, Vsn, undefined}, {Repo, Author, Tag}, Config);
+alt_load(DepsDir, {App, Vsn, _}, {{scm, _Scm}, _Repo, _Checkout}=Scm, Config) ->
+    alt_load(DepsDir, App, Vsn, Scm, Config);
 alt_load(DepsDir, {App, Vsn, _}, {Repo, Author, Tag}, Config) ->
-    rebar_log:log(debug, "Loading ~p-~p from ~p~n",
+    alt_load(DepsDir, App, Vsn, make_url(Repo, Author, App, Tag), Config).
+
+alt_load(DepsDir, App, Vsn, {{scm, _}, Repo, _}=Scm, Config) ->
+    rebar_log:log(debug, "Attempting to load ~p-~s from ~p~n",
+                  [App, Vsn, Repo]),
+    AppName = atom_to_list(App) ++ "-" ++ Vsn,
+    Target = filename:join(DepsDir, AppName),
+    rebar_log:log(debug, "scm target: ~p~n", [Target]),
+    case alt_load(Scm, Target, Config) of
+        {dir, Target} ->
+            ok;
+        Other ->
+            rebar_log:log(debug, "Unexpected scm load result: ~p~n", [Other]),
+            Other
+    end;
+alt_load(DepsDir, App, Vsn, Repo, Config) ->
+    rebar_log:log(debug, "Attempting to load ~p-~s from ~p~n",
                   [App, Vsn, Repo]),
     WorkDir = filename:join(rebar_utils:get_cwd(),
                     rebar_config:get_local(Config, alt_tmp_dir, ".altdeps")),
     AppName = atom_to_list(App) ++ "-" ++ Vsn,
-    Target = filename:join(WorkDir, Author ++ "-" ++ AppName),
-    Url = make_url(Repo, Author, App, Tag),
-    case fetch(Url, Target, Config) of
-        {error, Error} ->
-            rebar_log:log(warn, "Unable to load ~s: ~p~n", [AppName, Error]);
-        {ok, Target} ->
+    Target = filename:join(WorkDir, AppName),
+    case alt_load(Repo, Target, Config) of
+        {zip, Target} ->
+            rebar_log:log(debug, "Extracting ~s~n", [Target]),
             case zip:extract(Target, [memory]) of
                 {error, Reason} ->
                     rebar_log:log(warn,
                                   "Unable to extract archive ~s: ~p~n",
                                   [Target, Reason]);
                 {ok, FileList} ->
-                    Dest = filename:join(DepsDir, atom_to_list(App)),
-                    [ file:write_file(rename(Path, Dest), Data, [write]) ||
-                                                    {Path, Data} <- FileList ],
-                    %[ rebar_file_utils:mv(F,
-                    %    rename(F, Dest, WorkDir)) || F <- FileList ],
-                    %case Skip of
-                    %    undefined ->
-                    %        ok;
-                    %    _ ->
-                    %        {exclude, filename:basename(Dest)}
-                    %end
-                    
+                    Dest = filename:join(DepsDir,
+                                            atom_to_list(App) ++ "-" ++ Vsn),
+                    [ write(Path, Dest, Data) || {Path, Data} <- FileList ],
                     ok
-            end
+            end;
+        Other ->
+            rebar_log:log(debug, "Unexpected load result: ~p~n", [Other]),
+            Other
     end.
+
+
+alt_load(Repo, Target, Config) ->
+    case fetch(Repo, Target, Config) of
+        {error, Error} ->
+            rebar_log:log(warn, "Unable to fetch ~s: ~p~n", [Repo, Error]);
+        Other ->
+            Other
+    end.
+
+write(Path, Dest, Data) ->
+    Renamed = rename(Path, Dest),
+    rebar_log:log(debug, "Writing ~s to ~s~n", [Path, Renamed]),
+    file:write_file(Renamed, Data, [write]).
 
 rename(Path, Dest) ->
     [_|P] = filename:split(Path),
     Target = filename:join(Dest, filename:join(P)),
-    rebar_utils:ensure_dir(Target),
+    ok = rebar_utils:ensure_dir(Target),
     Target.
-
-%rename(FN, Dest, WorkDir) ->
-%    NewFN = filename:join([Dest] ++
-%        erlang:tl(filename:split(FN -- (WorkDir ++ "/" )))),
-%    rebar_utils:ensure_dir(NewFN),
-%    NewFN.
 
 make_url(bitbucket, Author, App, Vsn) ->
     "https://bitbucket.org/" ++ Author ++ "/" ++
@@ -203,11 +251,6 @@ make_url(bitbucket, Author, App, Vsn) ->
 make_url(github, Author, App, Vsn) ->
     "https://github.com/" ++ Author ++ "/" ++
     atom_to_list(App) ++ "/zipball/" ++ Vsn.
-
-%matching_dirs(F, ManagedDeps) ->
-%    lists:filter(fun({App, _Vsn}) ->
-%                    lists:prefix(atom_to_list(App), F)
-%                end, ManagedDeps).
 
 is_app_available(App, VsnRegex, Path) ->
     rebar_log:log(debug, "Searching for ~p(~~ ~s) in ~s~n", [App, VsnRegex, Path]),
@@ -232,7 +275,28 @@ is_app_available(App, VsnRegex, Path) ->
             {false, {missing_app_file, Path}}
     end.
 
-fetch(Url, Target, Config) ->
+fetch({{scm, git}, Repo, {tag, Tag}}, Target, Config) ->
+    case fetch({git, Repo, "HEAD"}, Target, Config) of
+        {dir, Target} ->
+            case rebar_utils:sh("git checkout -q " ++ Tag, [{cd, Target}]) of
+                {ok, _} -> {dir, Target};
+                Err -> Err
+            end;
+        Err ->
+            Err
+    end;
+fetch({{scm, git}, Repo, Branch}, Target, _Config) ->
+    rebar_utils:ensure_dir(Target),
+    case rebar_utils:sh("git clone -b " ++ Branch ++
+                        " " ++ Repo ++ " " ++ filename:basename(Target),
+                        [{cd, filename:dirname(Target)}]) of
+        {ok, _} ->
+            {dir, Target};
+        Err ->
+            rebar_log:log(warn, "failed to clone repo ~s: ~p~n", [Repo, Err]),
+            Err
+    end;
+fetch(("http" ++ _)=Url, Target, Config) ->
     case get({?MODULE, httpc}) of
         started ->
             ok;
@@ -262,7 +326,7 @@ fetch(Url, Target, Config) ->
                                      [{stream, Target}, {full_result, true}]) of
         {ok, saved_to_file} ->
             rebar_log:log(debug, "Successfully fetched tag...~n", []),
-            {ok, Target};
+            {zip, Target};
         {ok, {{_, 404, _}, _, _}} ->
             rebar_log:log(warn, "~s not found!~n", [Url]),
             {error, missing};
